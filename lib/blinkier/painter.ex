cacb035d8a2ff3f2baf365307bbb10b41db2281d
mmmries <riesmmm@gmail.com>
2020-06-10 13:04:07 -0600

    use joystick events to light up LEDs

--- lib/blinkier/painter.ex
+++ lib/blinkier/painter.ex
@@ -1,7 +1,7 @@
 defmodule Blinkier.Painter do
   use GenServer
   require Logger
-  @interval 100
+  @interval 20
 
   def start_link(i2c_name) do
     GenServer.start_link(__MODULE__, i2c_name)
@@ -9,13 +9,29 @@ defmodule Blinkier.Painter do
 
   def init(i2c_name) do
     {:ok, hts221} = HTS221.start_link(i2c_name)
-    {:ok, %{hts221: hts221}, @interval}
+    {:ok, %{hts221: hts221, count: 1}, @interval}
   end
 
   def handle_info(:timeout, state) do
     if screen_ready?() do
       paint(state)
     end
+    state = listen_to_joystick(state)
+    {:noreply, state, @interval}
+  end
+
+  def handle_info({:input_event, _joystick, [{:ev_key, :key_up, 1}]}, state) do
+    state = %{state | count: state.count + 1}
+    {:noreply, state, @interval}
+  end
+
+  def handle_info({:input_event, _joystick, [{:ev_key, :key_down, 1}]}, state) do
+    state = %{state | count: state.count - 1}
+    {:noreply, state, @interval}
+  end
+
+  def handle_info(other, state) do
+    Logger.error("#{__MODULE__} received unexpected message: #{inspect(other)}")
     {:noreply, state, @interval}
   end
 
@@ -25,15 +41,17 @@ defmodule Blinkier.Painter do
   @screen "/dev/fb1"
 
   def paint(state) do
-    num_pixels = round((HTS221.humidity(state.hts221) / 100.0) * 64)
-    temp = HTS221.temperature(state.hts221, scale: :fahrenheit)
-    temp_ratio = clamp((temp - 75.0) / 20.0)
-    hue = 240 + trunc(temp_ratio * 119)
-    rgb = Chameleon.HSV.new(hue, 100, 90) |> Chameleon.Color.RGB.from()
-    pixel = rgb_to_pixel(rgb)
+    #num_pixels = round((HTS221.humidity(state.hts221) / 100.0) * 64)
+    #temp = HTS221.temperature(state.hts221, scale: :fahrenheit)
+    #temp_ratio = clamp((temp - 75.0) / 20.0)
+    #hue = 240 + trunc(temp_ratio * 119)
+    #rgb = Chameleon.HSV.new(hue, 100, 90) |> Chameleon.Color.RGB.from()
+    #pixel = rgb_to_pixel(rgb)
+    pixel = rgb_to_pixel(Chameleon.RGB.new(255, 0, 0))
+    num_pixels = state.count
     blank = :binary.copy(<<0, 0>>, 64 - num_pixels)
     pixels = :binary.copy(pixel, num_pixels)
-    File.write(@screen, [blank, pixels])
+    File.write(@screen, [pixels, blank])
   end
 
   def screen_ready? do
@@ -44,6 +62,16 @@ defmodule Blinkier.Painter do
   defp clamp(num) when num > 1.0, do: 1.0
   defp clamp(num), do: num
 
+  defp listen_to_joystick(%{joystick: _} = state), do: state
+  defp listen_to_joystick(state) do
+    if File.exists?("/dev/input/event1") do
+      {:ok, joystick} = InputEvent.start_link("/dev/input/event1")
+      Map.put(state, :joystick, joystick)
+    else
+      state
+    end
+  end
+
   def rgb_to_pixel(%Chameleon.RGB{r: r, g: g, b: b}) do
     use Bitwise, only_operators: true
     blue = (b >>> 3)
@@ -52,9 +80,4 @@ defmodule Blinkier.Painter do
     <<b1, b2>> = <<red::unsigned-integer-size(5), green::unsigned-integer-size(6), blue::unsigned-integer-size(5)>>
     <<b2, b1>>
   end
-    #blue = (b >>> 3) &&& 0x1f
-    #green = (g >>> 2) &&& 0x3f
-    #red = (r >>> 3) &&& 0x1f
-    #combine = red ||| green ||| blue
-    #<<combine::unsigned-integer-size(16)>>
 end
